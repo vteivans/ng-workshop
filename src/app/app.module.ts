2230dbecb5f163674b2022b1820124ea20047300
Viesturs Teivans <vteivans@gmail.com>
2019-03-28 13:04:05 +0200

    Angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { fakeBackendProvider } from './fake-backend.interceptor';
 import { HttpClientModule } from '@angular/common/http';
+import { CreateNoteComponent } from './create-note/create-note.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [fakeBackendProvider],
   bootstrap: [AppComponent]
